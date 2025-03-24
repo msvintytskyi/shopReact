319c93953d0ae087b8eb81f77b744bb5cc6c6f01
msvintytskyi <mykyta.svintytskyi@gmail.com>
2025-03-23 19:52:56 -0500

    Added products

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,7 +3,7 @@ import React from "react";
 function Item({item}) {
   return (
     <div className="item">
-      <img src={""} />
+      <img src={"./img/" + item.img} />
       <h2>{item.title}</h2>
       <p>{item.desc}</p>
       <b>{item.price}</b>
