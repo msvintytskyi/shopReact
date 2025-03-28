bfcd6e7b398baf748a52b2b1082c4173f2b67a1d
msvintytskyi <mykyta.svintytskyi@gmail.com>
2025-03-28 17:51:52 -0500

    added cart div

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaCartShopping } from "react-icons/fa6";
 
 function Header() {
+  const [cartOpen, setCartOpen] = useState(false);
+
   return (
     <header>
       <div>
-        <span className="logo">House Staff</span>
-        <ul className="nav">
+        <span className='logo'>House Staff</span>
+        <ul className='nav'>
           <li>About us</li>
           <li>Contacts</li>
           <li>Profile</li>
         </ul>
+        <FaCartShopping onClick={() => setCartOpen(!cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+
+        {cartOpen && (
+          <div className='shop-cart'>
+
+          </div>
+        )}
       </div>
-      <div className="presentation"></div>
+      <div className='presentation'></div>
     </header>
   );
 }
